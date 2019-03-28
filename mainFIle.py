ad0c8dd3c855c906ebd3bef721307073896fcec9
krissnnaa <krissnnaa1988@gmail.com>
2019-03-28 17:41:34 -0400

    tupple of date and comments

--- mainFIle.py
+++ mainFIle.py
@@ -6,8 +6,10 @@ from gensim.models import Word2Vec
 
 login_number = 0
 
-commentCollection=[]
-def load_issues( url=None, owner_name=None, repo_name=None, dest_path=None, authentication=None):
+commentCollection = []
+
+
+def load_issues(url=None, owner_name=None, repo_name=None, dest_path=None, authentication=None):
     i = 0
     global login_number
     global commentCollection
@@ -59,82 +61,93 @@ def load_issues( url=None, owner_name=None, repo_name=None, dest_path=None, auth
         with open(dest_path + "/" + repo_name + ".json", "w") as infile:
             json.dump(output_list, infile)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
 
     with open("authentication.json") as outfile:
         authentication = json.load(outfile)
 
     dest_path = "/home/krishna/PycharmProjects/CommitCollection/pullFolder/"
-    readingPull=0
-    if readingPull==1:
-        project_={'ipython':'ipython'}
+    readingPull = 0
+    if readingPull == 1:
+        project_ = {'ipython': 'ipython'}
 
-        for key,value in project_.items():
-            load_issues(None,key, value, dest_path, authentication)
+        for key, value in project_.items():
+            load_issues(None, key, value, dest_path, authentication)
 
-    with open(dest_path+"ipython.json") as f:
-        file=json.load(f)
+        with open(dest_path + "ipython.json") as f:
+            file = json.load(f)
 
-    commentLinks=[]
+        commentLinks = []
 
-    for itemList in  file:
-        for index in range(len(itemList)):
-            createdDate=re.findall(r'^\d+',itemList[index]['created_at'])
-            if int(createdDate[0])<2018:
-                continue
-            else:
+        for itemList in file:
+            for index in range(len(itemList)):
+                createdDate = re.findall(r'^\d+', itemList[index]['created_at'])
+                if int(createdDate[0]) < 2018:
+                    continue
+                else:
 
-                commentLinks.append(itemList[index]['_links']['review_comments']['href'])
+                    commentLinks.append(itemList[index]['_links']['review_comments']['href'])
 
-    with open(dest_path + "commentsFrom2018.txt",'w') as f:
-        f.write(str(commentLinks))
+        with open(dest_path + "commentsFrom2018.txt", 'w') as f:
+            f.write(str(commentLinks))
 
-    if readingPull==1:
         for item in commentLinks:
-            load_issues(item,None,None,dest_path,authentication)
+            load_issues(item, None, None, dest_path, authentication)
 
         with open(dest_path + "/" + "commentsCollection" + ".json", "w") as f:
             json.dump(commentCollection, f)
 
-        with open(dest_path+"commentsCollection.json") as f:
-            file=json.load(f)
-
-        file = [x for x in file if len(x) > 0]
-
-        memberComment={}
-        for itemList in file:
-            for insideItem in itemList:
-                for index in range(len(insideItem)):
-                    authorAssociation = insideItem[index]['author_association']
-                    memberName=insideItem[index]['user']['login']
-                    commentBody=insideItem[index]['body'] + ' '
-                    flag=0
-                    initialComment=[]
-                    if authorAssociation == 'MEMBER':
-                        for key,value in memberComment.items():
-                            if key == memberName:
-                                previousComment=memberComment[memberName]
-                                previousComment.append(commentBody)
-                                memberComment[key]=previousComment
-                                flag=1
-                                break
-
-                        if flag==0:
-                            initialComment.append(commentBody)
-                            memberComment[memberName] = initialComment
-
-        with open(dest_path + "individualComments.json",'w') as f:
-            json.dump(memberComment,f)
-
-    with open(dest_path + "individualComments.json") as f:
+    with open(dest_path + "commentsCollection.json") as f:
         file = json.load(f)
 
-    preProcessComments={}
-    for key,value in file.items():
-        joinedValue=' '.join(value)
-        valueString=(re.sub(r'[^\w\s]', '', joinedValue)).lower()
-        tokenWords=nltk.tokenize.word_tokenize(valueString)
-        preProcessComments[key]=tokenWords
-        model = Word2Vec([tokenWords], window=6, size=310,negative=5, min_count=1)
-        model.wv.save_word2vec_format(dest_path+key+'.txt', binary=False)
-  
+    file = [x for x in file if len(x) > 0]
+
+    memberComment = {}
+    for itemList in file:
+        for insideItem in itemList:
+            for index in range(len(insideItem)):
+                authorAssociation = insideItem[index]['author_association']
+                memberName = insideItem[index]['user']['login']
+                commentBody = insideItem[index]['body'] + ' '
+                createdDate = insideItem[index]['created_at']
+                flag = 0
+                initialComment = []
+                if authorAssociation == 'MEMBER':
+                    for key, value in memberComment.items():
+                        if key == memberName:
+                            previousComment = memberComment[memberName]
+                            commentTuple = (createdDate, commentBody)
+                            previousComment.append(commentTuple)
+                            memberComment[key] =previousComment
+                            flag = 1
+                            break
+
+                    if flag == 0:
+                        commentTuple = (createdDate, commentBody)
+                        initialComment.append(commentTuple)
+                        memberComment[memberName] = initialComment
+
+    for key,value in memberComment.items():
+
+        fileRead=open(dest_path +key +"_data.txt", 'w')
+        fileRead.write('Created Date\t\t\t Comments\n')
+        for eachTuple in value:
+            preProcess=(re.sub(r'[^\w\s][\r\n]+', '', eachTuple[1])).lower()
+            fileRead.write('%s\t\t\t%s\n'%(eachTuple[0],preProcess))
+
+    # with open(dest_path + "individualComments.json", 'w') as f:
+    #     json.dump(memberComment, f)
+    #
+    # with open(dest_path + "individualComments.json") as f:
+    #     file = json.load(f)
+    #
+    # preProcessComments = {}
+    # for key, value in file.items():
+    #     joinedValue = ' '.join(value)
+    #     valueString = (re.sub(r'[^\w\s]', '', joinedValue)).lower()
+    #     tokenWords = nltk.tokenize.word_tokenize(valueString)
+    #     preProcessComments[key] = tokenWords
+    #     model = Word2Vec([tokenWords], window=6, size=310, negative=5, min_count=1)
+    #     model.wv.save_word2vec_format(dest_path + key + '.txt', binary=False)
+    # print('test')
