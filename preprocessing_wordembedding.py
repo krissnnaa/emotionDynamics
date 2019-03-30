92048e6c2fe083fee77a5ddb598af41699ada98c
krissnnaa <krissnnaa1988@gmail.com>
2019-03-30 16:21:18 -0400

    Delete preprocessing_wordembedding.py

--- preprocessing_wordembedding.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import Word2Vec
-import nltk
-
-mysentence="I am a student."
-token_words=nltk.tokenize.word_tokenize(mysentence)
-model=Word2Vec([token_words],window=2,size=10, min_count=1)
-print(model.wv.vocab)
-print(model.wv.vectors)
-model.wv.save_word2vec_format('model.txt', binary=False)
